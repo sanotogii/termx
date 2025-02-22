5a303b2d837875f1bdf3312ac811027c8d730d75
sanotogii <nasaereh@gmail.com>
2025-02-22 16:10:28 +0100

    Enhance chat input handling and add clear command functionality

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import google.generativeai as genai
 from config import Config
 import readline
+import os
 
 config = Config()
 
@@ -19,18 +20,20 @@ chat_session = model.start_chat(
 
 try:
     while True:
-        input_msg = input("> ")
-        msg =input_msg + ", keep it short, OS=UBUNTU, no markdown, if steps use ordered list, do not be very specific"
+        input_msg = input("❯ ")
+        msg =input_msg + ", keep it short, OS=UBUNTU, no markdown, if steps use ordered list, do not be very specific, give cli solutions"
         if input_msg:
-            readline.add_history(msg)
+            readline.add_history(input_msg)
         response = chat_session.send_message(msg)
         
         if msg.startswith("/help"):
             print("/q: quit")
+        elif msg.startswith("clear"):
+            os.system("clear")
         elif msg.startswith("/q" or "/quit"):
-            print("\nbye!")
+            print("bye!")
             break
         else:
             print(response.text)
 except KeyboardInterrupt:
-    print('\nbye!')
+    print('bye!')
